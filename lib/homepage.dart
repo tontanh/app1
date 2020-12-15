9b7dfa9236b8d332fc2f0ca3e39e7e4fdc88cbd8
tontanh <tonkmnv@gmail.comm>
2020-12-15 23:06:04 +0700

    new commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'flutter homepage ton',
+      title: 'flutter homepage ton2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
