0580c59e5297b098ec6c00eb456237dc524d0fb1
tontanh <tonkmnv@gmail.comm>
2020-12-17 13:36:42 +0700

    new one

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,43 @@
-import 'package:app1/page1.dart';
 import 'package:flutter/material.dart';
 
-void mian() {
-  runApp(MaterialApp(
-    home: Myapp(),
-  ));
+void main() {
+  runApp(MyApp());
 }
 
-class Myapp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  _MyappState createState() => _MyappState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(title: 'Flutter food ton app ap apapp app '),
+    );
+  }
 }
 
-class _MyappState extends State<Myapp> {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
       body: Center(
-          child: FlutterLogo(
-        size: 400,
-      )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'page',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
