8f33554641f113639400a949ca0c4676775df954
tontanh <tonkmnv@gmail.com>
2020-12-27 15:59:31 +0700

    27/12/2020

---
+++ lib/ordering_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Ordering_page());
+}
+
+class Ordering_page extends StatefulWidget {
+  @override
+  _Ordering_pageState createState() => _Ordering_pageState();
+}
+
+class _Ordering_pageState extends State<Ordering_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
